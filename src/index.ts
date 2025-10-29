0a338298cfc9048002b7d3d622d3069898c4dfa9
YammD123 <siamqueen52@gmail.com>
2025-10-29 20:20:49 +0700

    chore: menambahkan api docs dengan OpenApi serta implementasi

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,14 @@
+import openapi from "@elysiajs/openapi";
 import { Elysia } from "elysia";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia().get("/", () => "Hello Elysia").use(openapi({
+  documentation:{
+    info: {
+      title: "KasirApp API",
+      version: "1.0.0"
+    }
+  }
+})).listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
